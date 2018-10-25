74fcafe943473980b5b7c0e793e6bc0659215cc0
sbishop7 <seth.bishop@gmail.com>
2018-10-24 17:24:40 -0700

    add timer timestamp

--- script.js
+++ script.js
@@ -3,17 +3,28 @@ console.log("Hello World");
 const jsContainer = document.getElementById("js");
 const reactContainer = document.getElementById("react");
 
-jsContainer.innerHTML = `
-    <div class="demo">
-        Hello JS
-    </div>
-`;
+const render = () => {
+    jsContainer.innerHTML = `
+        <div class="demo">
+            Hello JS
+            <input />
+            <p>${new Date()}</p>
+        </div>
+    `;
+    ReactDOM.render(
+        React.createElement(
+            "div", 
+            { className: "demo" },
+            "Hello React",
+            React.createElement("input"),
+            React.createElement(
+                "p",
+                null,
+                new Date().toString()
+            )
+        ),
+        reactContainer
+    )
+}
 
-ReactDOM.render(
-    React.createElement(
-        "div", 
-        { className: "demo" },
-        "Hello React"
-    ),
-    reactContainer
-)
+setInterval(render, 1000);
